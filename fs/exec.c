e4084f7cb8e438d94ee44b14b87b079c2620d8a4
John Galt <johngaltfirstrun@gmail.com>
2021-12-16 11:12:35 -0500

    fs/exec: Force dex2oat64 on big cluster
    UpdateEngine behavior is to have 4 low priority threads here, let's
    attempt to speed it up slightly
    
    Change-Id: I4c4910d2e2ffd45be1fd3a39b081bc0edb542131

--- fs/exec.c
+++ fs/exec.c
@@ -79,7 +79,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define SFLINGER_BIN_PREFIX "/system/bin/surfaceflinger"
-
+#define DEX2OAT64_BIN_PREFIX "/apex/com.android.art/bin/dex2oat64"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1893,6 +1893,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   SFLINGER_BIN_PREFIX,
 					   strlen(SFLINGER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					   DEX2OAT64_BIN_PREFIX,
+					   strlen(DEX2OAT64_BIN_PREFIX)))) {
+			current->pc_flags |= PC_PERF_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 		else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
